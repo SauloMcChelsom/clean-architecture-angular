2c9b8c6e752b6de33ff7a84ed6b09d8d7a6b054d
Saulo McChelsom <2033.xyz@gmail.com>
2024-11-05 19:09:28 -0300

    base

--- src/app/ui/feature/showcase/chip-option-showcase/chip-option-showcase.component.ts
+++ src/app/ui/feature/showcase/chip-option-showcase/chip-option-showcase.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ChipOptionComponent } from 'src/app/ui/components/chip-option/chip-option.component';
+import { ChipOptionModel } from 'src/app/ui/components/chip-option/models';
 
 @Component({
   selector: 'app-chip-option-showcase',
@@ -12,11 +14,31 @@ import { ChipOptionComponent } from 'src/app/ui/components/chip-option/chip-opti
     ChipOptionComponent
   ]
 })
-export class ChipOptionShowcaseComponent implements OnInit {
+export class ChipOptionShowcaseComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  config = {
+    formControl: new FormControl(['one_fish']),
   }
 
+  chipList:ChipOptionModel[] = [
+    { label: 'One fish', cod: 'one_fish',  color: 'primary', disabled: false },
+    { label: 'Two fish', cod: 'two_fish',  color: 'primary', disabled: false },
+    { label: 'Accent fish', cod: 'accent_fish',  color: 'primary', disabled: false },
+    { label: 'Warn fish', cod: 'warn_fish',  color: 'warn', disabled: true },
+  ];
+
+  @ViewChild(ChipOptionComponent) InputComponent!: ChipOptionComponent;
+
+
+  reset(){
+    this.InputComponent.resetToInitialState()
+  }
+
+  disable(){
+    this.InputComponent.disable()
+  }
+
+  enable(){
+    this.InputComponent.enable()
+  }
 }
